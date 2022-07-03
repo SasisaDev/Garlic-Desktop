239054b46463f59c47016906bd793dc301a4e2a5
Vskami <deadlybridge@mail.ru>
2022-07-03 03:55:21 +0300

    03.07.2022 3:55

--- src/client/Components/Guild/GuildList/GuildList.tsx
+++ src/client/Components/Guild/GuildList/GuildList.tsx
@@ -5,7 +5,15 @@ import "./GuildList.scss"
 export default function GuildList() {
     return (
         <div className="GuildListBody">
+            <div className="GuildListButtonPlace">
+            <div className="Guild"></div>
+            </div>
+            <div className="GuildListList">
 
+            </div>
+            <div className="GuildListButtonPlace">
+                <div className="Guild"></div>
+            </div>
         </div>
     )
 } 
--- src/client/Components/Guild/Selector/Selector.tsx
+++ src/client/Components/Guild/Selector/Selector.tsx
@@ -24,10 +24,11 @@ export default function Selector() {
             <GuildSelector />
             <div className="UserBody">
                 <div className="UserAvatar">
-
+                    <div className="UserAvatarIcon" />
                 </div>
                 <div className="UserState">
-
+                    <h6 className="Text">Name</h6>
+                    <h6 className="Text">Status</h6>
                 </div>
                 <div className="UserFunctionals">
 
