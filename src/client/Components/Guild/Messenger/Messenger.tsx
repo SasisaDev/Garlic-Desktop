52272115738a9a472beb01245fb67b4ba843343f
Vskami <deadlybridge@mail.ru>
2022-07-17 22:03:43 +0300

    17.07.2022 22:03

--- src/client/Components/Guild/Messenger/Messenger.tsx
+++ src/client/Components/Guild/Messenger/Messenger.tsx
@@ -5,6 +5,14 @@ import Message from "./Message"
 import "./Messenger.scss"
 
 export default function Messenger({channel}) {
+
+    function AutoGrow() {
+        let element = (document.getElementById("inputChannel") as HTMLTextAreaElement);
+
+        element.style.height = "28px";
+        element.style.height = ((document.getElementById("inputChannel") as HTMLTextAreaElement).scrollHeight)+"px";
+    }
+
     return (
         <div className="MessengerBody">
             <div className="MessengerMessages">
@@ -12,7 +20,7 @@ export default function Messenger({channel}) {
                 <Message message={null}></Message>
             </div>
             <div className="MessengerInputBody">
-                <textarea className="MessengerInput"></textarea>
+                <textarea className="MessengerInput" id="inputChannel" onInput={AutoGrow}></textarea>
                 <div className="MessengerInputSend">{'>'}</div>
             </div>
         </div>
